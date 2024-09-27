84467c5e908ba412cd4271f2fc8dbe21d50eadb3
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-09-27 22:53:03 +0600

    hide spinner

--- src/app/(default)/layout.js
+++ src/app/(default)/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <NextTopLoader  />
+        <NextTopLoader showSpinner={false} />
         <Navbar />
         {children}
       </body>
